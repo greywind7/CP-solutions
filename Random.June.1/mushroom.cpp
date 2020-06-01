af141c085ba620fcdceabd513bf098b8e2a6d37f
greywind7 <ritik.aggr@gmail.com>
2020-06-01 12:58:07 +0530

    mushroom problem done

---
+++ Random.June.1/mushroom.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    map<int,pair<int,int> > scores;
+    vector<string> names(n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        int oregano, boletus, jungle, score;
+        cin >> names[i];
+        cin >> oregano >> boletus >> jungle;
+        score = -1 * (5*oregano + 3*boletus + 15*jungle);    
+        scores[score].first++;
+        scores[score].second = i;
+    }
+
+    auto it = scores.begin();
+    while(it->first != 1 && it != scores.end()) it++;
+    cout << names[it->second.second];
+    return 0;
+    cout << (it == scores.end() ? "all disqualified" : (string) names[it->second.second]);
+}
